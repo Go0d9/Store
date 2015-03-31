4b10a0bde66452f4118c4122d7cca96ef4feab8e
Go0d9 <flusherro404@gmail.com>
2015-04-01 01:05:12 +0300

    Log in and Sign up don't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resources :posts
   root 'posts#index'
   get  'home' => 'posts#indox' 
   resources :users
   get 'users/show'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
 end
